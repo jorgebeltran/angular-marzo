d31dfa94ed137db4b23db0f792e83153c018ec3c
jorgebeltran <yo_coqui@hotmail.com>
2018-03-08 18:40:22 -0400

    3.2 redireccionando con routelink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UserListComponent } from './user-list/user-list.component';
-import { Component } from '@angular/core/src/metadata/directives';
 import { UserFormCreateComponent } from './user-form-create/user-form-create.component';
+import { UserFormEditComponent } from './user-form-edit/user-form-edit.component';
 
 const routes: Routes = [
   {
@@ -16,15 +16,19 @@ const routes: Routes = [
   },
 
   {
-    path:'user/create',
-    component:UserFormCreateComponent
+    path: 'user/create',
+    component: UserFormCreateComponent
+  },
+  {
+    path: 'user/edit',
+    component: UserFormEditComponent
   },
 
-{
-  path:'',
-  redirectTo:'home',
-  pathMatch:'full'
-}
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
 
 ];
 
