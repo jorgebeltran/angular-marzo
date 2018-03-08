83cbb1944d6c6b906ced06640c75d001257a2e3b
jorgebeltran <yo_coqui@hotmail.com>
2018-03-08 17:56:47 -0400

    declaracion de rutas3.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { Component } from '@angular/core/src/metadata/directives';
+import { UserFormCreateComponent } from './user-form-create/user-form-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'user/list',
+    component: UserListComponent
+  },
+
+  {
+    path:'user/create',
+    component:UserFormCreateComponent
+  },
+
+{
+  path:'',
+  redirectTo:'home',
+  pathMatch:'full'
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
