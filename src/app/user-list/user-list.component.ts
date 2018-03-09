fa7cfee388fe49d3921192f2fc3ba4d8bb1713c7
jorgebeltran <yo_coqui@hotmail.com>
2018-03-09 17:20:32 -0400

    6.-data binding para muchos items

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -7,14 +7,28 @@ import { User } from '../model/user';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
-  test:User={
-    name:'gabriel',
+  userlist:User[]=[];
+
+  constructor() { 
+    this.userlist.push({
+      name:'gabriel',
     lastname:'meneces',
     username:'gabriel123',
-    email:'gabrie@gmail.com',
+    email:'gabrie@gmail.com'
+    });
+    this.userlist.push({
+      name:'gabriel1',
+    lastname:'meneces',
+    username:'gabriel123',
+    email:'gabrie@gmail.com'
+    });
+      this.userlist.push({
+      name:'gabriel2',
+    lastname:'meneces',
+    username:'gabriel123',
+    email:'gabrie@gmail.com'
+    });
   }
-
-  constructor() { }
 
   ngOnInit() {
   }
